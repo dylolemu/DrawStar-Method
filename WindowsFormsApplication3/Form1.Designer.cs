1bc0d572cf707c88e4b80bb53b2e1ee98f17ad5a
mitccase <mitccase854@ed.amdsb.ca>
2016-04-19 09:13:48 -0400

    drawStar program is completed. It draws a star and scales it size. The method created is also correctly finished. Comments are added. Only need to include the splash screen.

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Star
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Star));
+            this.xBox = new System.Windows.Forms.TextBox();
+            this.yBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sizeBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // xBox
+            // 
+            this.xBox.Location = new System.Drawing.Point(51, 72);
+            this.xBox.Name = "xBox";
+            this.xBox.Size = new System.Drawing.Size(46, 20);
+            this.xBox.TabIndex = 0;
+            // 
+            // yBox
+            // 
+            this.yBox.Location = new System.Drawing.Point(115, 72);
+            this.yBox.Name = "yBox";
+            this.yBox.Size = new System.Drawing.Size(54, 20);
+            this.yBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(99, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sizeBox
+            // 
+            this.sizeBox.Location = new System.Drawing.Point(186, 72);
+            this.sizeBox.Name = "sizeBox";
+            this.sizeBox.Size = new System.Drawing.Size(54, 20);
+            this.sizeBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X                      Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SIZE";
+            // 
+            // Star
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sizeBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.yBox);
+            this.Controls.Add(this.xBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Star";
+            this.Text = "Star";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox xBox;
+        private System.Windows.Forms.TextBox yBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox sizeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,64 @@
+﻿//Created by Dylon Lemus
+//April 19, 2016
+//Description: A program which draws a star in a location and size inputed. Also includes a method which can be used in any computer to draw the star.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Star : Form
+    {
+        public Star()
+        {
+            InitializeComponent();
+        }
+        float xVal;//location
+        float yVal;//location
+        float size;//lenght and width
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            xVal = Convert.ToSingle(xBox.Text);
+            yVal = Convert.ToSingle(yBox.Text);
+            size = Convert.ToSingle(sizeBox.Text);
+            xBox.Hide();
+            yBox.Hide();
+            sizeBox.Hide();
+            label1.Hide();
+            label2.Hide();
+            Pen drawPen = new Pen(Color.Red, 2);
+            DrawStar(drawPen,xVal,yVal,size);
+        }
+
+        /// <summary>
+        /// This method draws a star of whatever size you want it, in whichever location you want it. 
+        /// The color of the star as well as the thickness of the line is also customizable.
+        /// </summary>
+        /// <param name="drawPen">This is the pen that is used to draw the star.</param>
+        /// <param name="x">This is the x-coordinate of the star.</param>
+        /// <param name="y">This is the y-cordinate of the star.</param>
+        /// <param name="size">This is the lenght and width of the star.</param>
+        public void DrawStar(Pen drawPen,float x, float y, float size)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            formGraphics.DrawLine(drawPen, x + size / 2, y, x + size * 383333 / 1000000, y + size * 329 / 1000);
+            formGraphics.DrawLine(drawPen, x + size / 2, y, x + size * 60833 / 100000, y + size * 329 / 1000);
+            formGraphics.DrawLine(drawPen, x, y + size * 329 / 1000, x + size * 383333 / 1000000, y + size * 329 / 1000);
+            formGraphics.DrawLine(drawPen, x + size, y + size * 329 / 1000, x + size * 60833 / 100000, y + size * 329 / 1000);
+            formGraphics.DrawLine(drawPen, x, y + size * 329 / 1000, x + size * 304166 / 1000000, y + size * 575 / 1000);
+            formGraphics.DrawLine(drawPen, x + size, y + size * 329 / 1000, x + size * 6875 / 10000, y + size * 575 / 1000);
+            formGraphics.DrawLine(drawPen, x + size * 170833 / 1000000, y + size, x + size * 304166 / 1000000, y + size * 575 / 1000);
+            formGraphics.DrawLine(drawPen, x + size * 8291 / 10000, y + size, x + size * 6875 / 10000, y + size * 575 / 1000);
+            formGraphics.DrawLine(drawPen, x + size * 170833 / 1000000, y + size, x + size / 2, y + size * 729 / 1000);
+            formGraphics.DrawLine(drawPen, x + size * 8291 / 10000, y + size, x + size / 2, y + size * 729 / 1000);
+        }
+
+    }
+}
