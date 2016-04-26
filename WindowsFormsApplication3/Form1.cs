b17ab0fc6598bb0053e6dadbe92d866bbf581e3b
mitccase <mitccase854@ed.amdsb.ca>
2016-04-26 08:47:35 -0400

    FInished DrawStar method and program. All comments are included and splashscreen is also included. Star's size adjust appropriately and method is working well.

--- WindowsFormsApplication3/Form1.cs
+++ WindowsFormsApplication3/Form1.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace WindowsFormsApplication3
 {
@@ -18,11 +19,72 @@ namespace WindowsFormsApplication3
         public Star()
         {
             InitializeComponent();
+            splashScreen();
         }
         float xVal;//location
         float yVal;//location
         float size;//lenght and width
 
+        public void splashScreen()
+        {
+            Form ss = new Form();
+            ss.Size = new Size(400, 300);
+            ss.FormBorderStyle = FormBorderStyle.None;
+            ss.StartPosition = FormStartPosition.CenterScreen;
+            ss.BackColor = Color.Black;
+            Graphics ssGraphics = ss.CreateGraphics();
+            Pen ssPen = new Pen(Color.White, 10);
+            SolidBrush ssBrush = new SolidBrush(Color.White);
+            SolidBrush ssBrush1 = new SolidBrush(Color.Black);
+
+            PictureBox logoPic = new PictureBox();
+            logoPic.BackgroundImage = Properties.Resources.logo;
+            logoPic.Size = new Size(150, 100);
+            logoPic.BackColor = Color.Transparent;
+            logoPic.BackgroundImageLayout = ImageLayout.Zoom;
+            logoPic.Location = new Point(ss.Width / 2 - logoPic.Width / 2, ss.Height / 2 - logoPic.Height / 2);
+            ss.Controls.Add(logoPic);
+
+            logoPic.Hide();
+            ss.Show();
+
+
+
+            for (int i = 50; i < 150; i++)
+            {
+
+                ssGraphics.FillRectangle(ssBrush, (i - 50) * 2, 10, 50, 50);
+                ssGraphics.FillRectangle(ssBrush, 450 - (i * 2), 10, 50, 50);
+
+                ssGraphics.FillRectangle(ssBrush, i - 50, 130, 50, 50);
+                ssGraphics.FillRectangle(ssBrush, 400 - i, 130, 50, 50);
+
+                ssGraphics.FillRectangle(ssBrush, (i - 50) * 2, 240, 50, 50);
+                ssGraphics.FillRectangle(ssBrush, 450 - (i * 2), 240, 50, 50);
+
+                ssGraphics.DrawLine(ssPen, 0, 0, i * 2, i * 2);
+                ssGraphics.DrawLine(ssPen, 400, 400, 400 - (i * 2), 400 - (i * 2));
+
+                ssGraphics.DrawLine(ssPen, 0, 400, (i * 2), 400 - (i * 2));
+                ssGraphics.DrawLine(ssPen, 400, 0, 400 - (i * 2), (i * 2));
+                ssGraphics.DrawLine(ssPen, 200, 0, 200, i);
+                ssGraphics.DrawLine(ssPen, 200, 300, 200, 300 - i);
+
+
+                logoPic.Show();
+                Thread.Sleep(10);
+
+            }
+            logoPic.Show();
+
+            ss.Refresh();
+            ssGraphics.FillRectangle(ssBrush, 0, 80, 400, 15);
+
+            ssGraphics.FillRectangle(ssBrush, 0, 220, 400, 15);
+
+            Thread.Sleep(1500);
+            ss.Hide();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             xVal = Convert.ToSingle(xBox.Text);
